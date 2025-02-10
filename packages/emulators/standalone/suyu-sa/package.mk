c3de38d209cdf9c20c5b266819e0171e522b9673
Kdog <cde@blackwolf>
2025-02-10 14:54:26 -0500

    Add settings in ES, upgrade mesa for better perf, replace logo

--- packages/emulators/standalone/suyu-sa/package.mk
+++ packages/emulators/standalone/suyu-sa/package.mk
@@ -11,26 +11,24 @@ PKG_SITE="https://git.suyu.dev/suyu/suyu"
 PKG_URL="${PKG_SITE}.git"
 PKG_VERSION="ee365bad9501c73ff49936e72ec91cd9c3ce5c24"
 
-PKG_CMAKE_OPTS_TARGET+="        -DENABLE_QT6=ON \
-                                -DCMAKE_BUILD_TYPE=Release \
-                                -DSUYU_ROOM=OFF \
-                                -DSUYU_USE_BUNDLED_SDL2=OFF \
-                                -DSUYU_USE_BUNDLED_QT=OFF \
-                                -DSUYU_TESTS=OFF \
-                                -DENABLE_SDL2=ON \
-                                -DARCHITECTURE_x86_64=OFF \
-                                -DARCHITECTURE_arm64=ON \
-                                -DBUILD_SHARED_LIBS=OFF \
-																-DENABLE_WEB_SERVICE=OFF \
-																-DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
-																-DSUYU_USE_BUNDLED_FFMPEG=OFF \
-																-DSUYU_USE_EXTERNAL_VULKAN_HEADERS=ON \
-																-DSUYU_USE_EXTERNAL_SDL2=ON \
-																-DSUYU_USE_FASTER_LD=OFF \
-																-DSUYU_USE_PRECOMPILED_HEADERS=ON \
-																-DSUYU_USE_QT_MULTIMEDIA=OFF \
-																-DSUYU_USE_QT_WEB_ENGINE=OFF \
-																-DUSE_DISCORD_PRESENCE=OFF"
+PKG_CMAKE_OPTS_TARGET+="-DENABLE_QT=ON \
+                    -DENABLE_QT6=ON \
+                    -DCMAKE_BUILD_TYPE=Release \
+                    -DSUYU_ROOM=OFF \
+                    -DSUYU_USE_BUNDLED_SDL2=OFF \
+                    -DSUYU_USE_BUNDLED_QT=OFF \
+                    -DSUYU_TESTS=OFF \
+                    -DENABLE_SDL2=ON \
+                    -DARCHITECTURE_x86_64=OFF \
+                    -DARCHITECTURE_arm64=ON \
+                    -DBUILD_SHARED_LIBS=OFF \
+                    -DENABLE_WEB_SERVICE=OFF \
+                    -DSUYU_USE_BUNDLED_FFMPEG=OFF \
+                    -DSUYU_USE_FASTER_LD=OFF \
+                    -DSUYU_USE_QT_MULTIMEDIA=OFF \
+                    -DSUYU_USE_QT_WEB_ENGINE=OFF \
+                    -DSUYU_ENABLE_LTO=ON \
+                    -DUSE_DISCORD_PRESENCE=OFF"
 
 pre_configure_target() {
   CFLAGS=$(echo ${CFLAGS} | sed -e "s|-Ofast|-O3|")
@@ -40,8 +38,6 @@ pre_configure_target() {
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
     cp ${PKG_BUILD}/.${TARGET_NAME}/bin/suyu  ${INSTALL}/usr/bin/
-    cp ${PKG_BUILD}/.${TARGET_NAME}/bin/suyu-cmd  ${INSTALL}/usr/bin/
-    #cp ${PKG_BUILD}/.${TARGET_NAME}/bin/suyu-room  ${INSTALL}/usr/bin/
 		cp -rf ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
     chmod +x ${INSTALL}/usr/bin/start_suyu.sh
 
